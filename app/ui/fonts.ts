c355a57c09b4f7885f49bbae40bb4af5abe451ac
AlphaMikey23 <eng22cse001@gmail.com>
2025-10-17 11:12:55 +0530

    Added Roboto google font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
 import { Inter , Lusitana } from 'next/font/google';
+import { Roboto } from 'next/font/google';
+
+export const roboto = Roboto({
+    weight: ['400','700']
+})
 
 export const inter = Inter( { subsets: ['latin']});
 
