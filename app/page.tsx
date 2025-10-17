4e87f263a22666fdeda5d0085b5743211dca3262
Jay <eng22cse001@gmail.com>
2025-10-17 20:57:20 +0530

    Added button using css modules

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,10 @@ import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
+    
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        
         { <AcmeLogo /> }
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -33,6 +35,8 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        
+        
           {/* Add Hero Images Here */}
           <Image 
           src="/hero-desktop.png"
@@ -51,6 +55,7 @@ export default function Page() {
           />
         </div>
       </div>
+      <button type="button" className={styles.error} > The website is still under construction!!</button>
     </main>
   );
 }
