d9842fa3e28495e66cf0031e8dc4f43983fa88de
AlphaMikey23 <eng22cse001@gmail.com>
2025-10-23 11:25:04 +0530

    Fixed Roboto subsets font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter , Lusitana } from 'next/font/google';
 import { Roboto } from 'next/font/google';
 
 export const roboto = Roboto({
-    weight: ['400','700']
+    weight: ['400','700'], subsets: ['latin']
 })
 
 export const inter = Inter( { subsets: ['latin']});
@@ -10,3 +10,5 @@ export const inter = Inter( { subsets: ['latin']});
 export const lusitana = Lusitana({
     weight: ['400', '700'], subsets: ['latin']
 });
+
+
