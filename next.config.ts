9617c81fa7f484e9354f1554616344c95b6acbfe
AlphaMikey23 <eng22cse001@gmail.com>
2025-10-24 11:32:08 +0530

    Trying to add search

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental'
-  }
+  cacheComponents: true,
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
   /* config options here */
 };
 
