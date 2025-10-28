483638144de27c7bcac81007e9cbd1ec321c0ad8
AlphaMikey23 <eng22cse001@gmail.com>
2025-10-28 16:23:49 +0530

    Fixed search

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
+
+
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
@@ -14,18 +16,21 @@ export default async function Page(props: {
     }>;
 }) {
     // 'use cache'
+    <Suspense> 
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    </Suspense>
 
     return (
+        <Suspense>
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
                 <h1 className={`${roboto.className} text-2xl`}>Invoices</h1>
         </div>
 
         <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-            <Search placeholder="Search invoices..." />
+            <Search />
             <CreateInvoice />
         </div>
 
@@ -38,6 +43,7 @@ export default async function Page(props: {
 
         </div>
         </div>
+        </Suspense>
         
     )
 }
