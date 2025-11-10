5e79c4c7c0dc8a3fdb78268dba24a2a85bd24b88
AlphaMikey23 <eng22cse001@gmail.com>
2025-11-10 17:37:46 +0530

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { roboto } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official next.js course dashboard application built with app router by Jay',
+  metadataBase: new URL('https://jay-nextjs-project.vercel.app'),
+}
 
 export default function RootLayout({
   children,
